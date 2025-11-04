33eacb975891a896081d8c8a8cfb81042092b780
Prafull Tupe <kumartupe@gmail.com>
2025-11-04 22:25:48 +0530

    Playing with Router Handler

--- src/app.js
+++ src/app.js
@@ -2,26 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ fistname: "Prafull", lastname: "Kumar" });
-});
+app.use("/user", [
+  (req, res, next) => {
+    res.send("Users endpoint1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("This always runs after the first middleware");
 
-app.post("/user", (req, res) => {
-  res.send("User created");
-});
+    next();
+    res.send("Users endpoint2");
+  },
+  (req, res) => {
+    console.log("This always runs after the second middleware");
+    res.send("Users endpoint3");
+  },
+]);
 
-app.delete("/user", (req, res) => {
-  res.send("User deleted");
-});
-
-app.use("/text", (req, res) => {
-  res.send("This is a text response.");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("Hello, World trst!");
-  next();
-});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
