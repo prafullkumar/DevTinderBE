42cfecb7140f136c071136a1114666448c383f7f
Prafull Tupe <kumartupe@gmail.com>
2025-11-04 16:57:27 +0530

    added user api call

--- src/app.js
+++ src/app.js
@@ -2,17 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.get("/user", (req, res) => {
+  res.send({ fistname: "Prafull", lastname: "Kumar" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("User created");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted");
+});
+
+app.use("/text", (req, res) => {
+  res.send("This is a text response.");
+});
+
+app.use("/", (req, res, next) => {
   res.send("Hello, World trst!");
   next();
 });
-app.get("/text", (req, res) => {
-  res.send("This is a text response.");
-});
-app.get("/hello", (req, res) => {
-  res.send("Hello from /hello route!");
-});
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
